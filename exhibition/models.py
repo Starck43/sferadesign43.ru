287025193a4d3144bc9c1982a2a960d7eb0b83e4
Starck43 <saloon.as@gmail.com>
2020-09-23 23:33:32 +0300

    Updated models structure

--- exhibition/models.py
+++ exhibition/models.py
@@ -28,13 +28,13 @@ def get_image_html(obj)
 	else:
 		return format_html('<img src="/media/no-image.png" width="50"/>')
 
-	"""Abstract model for Participant"""
+"""Abstract model for Participant"""
 class Participant(models.Model):
 	name = models.CharField('ФИО', max_length=100)
 	slug = models.SlugField('Ярлык', max_length=100, unique=True)
 	description = models.TextField('Описание', db_index=True, blank=True)
 	photo = models.ImageField('Аватар', upload_to=avatar_upload_folder, null=True, blank=True)
-	
+
 	class Meta:
 		ordering = ['name'] # '-' for DESC ordering
 
@@ -56,16 +56,24 @@ class Participant(models.Model):
 		photo_thumb.short_description = 'Портрет'
 
 
-class Exhibitors(Participant):
-	address = models.CharField('Адрес', max_length=150, blank=True)
+"""Abstract model for Contact"""
+class Contact(models.Model):
+	address = models.CharField('Адрес', max_length=100, blank=True)
 	phone = models.PhoneNumberField('Контактный телефон')
 	email = models.EmailField('E-mail', max_length=75)
-	site = models.URLField('Сайт', max_length=150)
-	vk = models.URLField('Вконтакте', max_length=150)
-	fb = models.URLField('Facebook', max_length=150)
-	instagram = models.URLField('Instagram', max_length=150)
-	
+	site = models.URLField('Сайт', max_length=75)
+	vk = models.URLField('Вконтакте', max_length=75)
+	fb = models.URLField('Facebook', max_length=75)
+	instagram = models.URLField('Instagram', max_length=75)
 	logo = models.ImageField('Логотип', upload_to=logo_upload_folder, null=True, blank=True)
+
+	def logo_thumb(self):
+		get_image_html(self.logo)
+
+	logo_thumb.short_description = 'Логотип'
+
+
+class Exhibitors(Participant, Contact):
 
 	# Metadata
 	class Meta(Participant.Meta):
@@ -77,89 +85,38 @@ class Exhibitors(Participant):
 		#return reverse('exhibitor-detail', kwargs={'slug': self.slug })
 
 
-	def logo_thumb(self):
-		get_image_html(self.logo)
-
-	logo_thumb.short_description = 'Логотип'
-
-
 class Jury(Participant):
-	
+
 	# Metadata
 	class Meta(Participant.Meta):
-		ordering = ['name'] # '-' for DESC ordering
 		verbose_name = 'Жюри'
-
+		verbose_name_plural = 'Жюри'
 
 	def get_absolute_url(self):
 		pass
 		#return reverse('Jury-detail', kwargs={'slug': self.slug })
 
-	
 
-class Partners(Participant):
-	address = models.CharField('Адрес', max_length=150, blank=True)
-	phone = models.PhoneNumberField('Контактный телефон')
-	email = models.EmailField('E-mail', max_length=75)
-	site = models.URLField('Сайт', max_length=150)
-	vk = models.URLField('Вконтакте', max_length=150)
-	fb = models.URLField('Facebook', max_length=150)
-	instagram = models.URLField('Instagram', max_length=150)
-	logo = models.ImageField('Логотип', upload_to=logo_upload_folder, null=True, blank=True)
+class Partners(Participant, Contact):
 
 	# Metadata
 	class Meta(Participant.Meta):
-		ordering = ['name'] # '-' for DESC ordering
 		verbose_name = 'Партнер выставки'
 		verbose_name_plural = 'Партнеры выставки'
-
 
 	def get_absolute_url(self):
 		pass
 		#return reverse('exhibitor-detail', kwargs={'slug': self.slug })
 
-	def logo_thumb(self):
-		get_image_html(self.logo)
 
-	logo_thumb.short_description = 'Логотип'
-
-
-class Nominations(models.Model):
-	name = models.CharField('Номинация', max_length=150)
-	slug = models.SlugField('Ярлык', max_length=150, unique=True)
-	description = models.TextField('Описание номинации', db_index=True, blank=True)
-	logo = models.ImageField('Логотип', upload_to=logo_upload_folder, null=True, blank=True)
-
-	# Metadata
-	class Meta:
-		ordering = ['name'] # '-' for DESC ordering
-		verbose_name = 'Номинация'
-		verbose_name_plural = 'Номинации'
-
-	def save(self, *args, **kwargs):
-		if not self.slug:
-			self.slug = uuslug(self.name, instance=self)
-		super().save(*args, **kwargs)
-
-	def __str__(self):
-		return self.name
-
-	def get_absolute_url(self):
-		pass
-		#return reverse('nomination-detail', kwargs={'slug': self.slug })
-
-	def logo_thumb(self):
-		get_image_html(self.logo)
-
-	logo_thumb.short_description = 'Логотип'
-
-
-class Exhibition(models.Model):
+''' Таблица Выставки '''
+class Exhibitions(models.Model):
 	title = models.CharField('Название выставки', max_length=150)
 	slug = models.SlugField('Ярлык', max_length=150, unique=True)
 	description = models.TextField('Описание выставки', db_index=True, blank=True)
 	date_start = DateField('Начало выставки', unique=True)
 	date_end = DateField('Окончание выставки', unique=True)
+	exhibitions = models.ManyToManyField(Exhibitions, db_table='nominations_relationships', on_delete=models.SET_NULL, related_name='exhibitions')
 
 	# Metadata
 	class Meta:
@@ -180,23 +137,54 @@ class Exhibition(models.Model):
 		#return reverse('exhibition-detail', kwargs={'slug': self.slug })
 
 
-class Events(models.Model):
-
-	exhibition = models.ForeignKey(Exhibition, on_delete=models.CASCADE, related_name='events', null=True, blank=True)
-	title = models.CharField('Название мероприятия', max_length=200)
-	description = models.TextField('Описание мероприятия', db_index=True)
-	date_event = DateField('Дата мероприятия')
-	time_start = TimeField('Начало мероприятия', unique=True)
-	time_end = TimeField('Окончание мероприятия', unique=True)
-	location = models.CharField('Зона проведения', max_length=150)
-	hoster = models.CharField('Участник мероприятия', max_length=100)
-	lector = models.CharField('Ведущий мероприятия', max_length=100)
+''' Таблица Номинаций '''
+class Nominations(models.Model):
+	title = models.CharField('Номинация', max_length=150)
+	slug = models.SlugField('Ярлык', max_length=150, unique=True)
+	description = models.TextField('Описание номинации', db_index=True, blank=True)
+	logo = models.ImageField('Логотип', upload_to=logo_upload_folder, null=True, blank=True)
 
 	# Metadata
 	class Meta:
 		ordering = ['title'] # '-' for DESC ordering
-		verbose_name = 'Мероприятие выставки'
-		verbose_name_plural = 'Мероприятия выставки'
+		verbose_name = 'Номинация'
+		verbose_name_plural = 'Номинации'
+
+	def save(self, *args, **kwargs):
+		if not self.slug:
+			self.slug = uuslug(self.title, instance=self)
+		super().save(*args, **kwargs)
+
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		pass
+		#return reverse('nomination-detail', kwargs={'slug': self.slug })
+
+	def logo_thumb(self):
+		get_image_html(self.logo)
+
+	logo_thumb.short_description = 'Логотип'
+
+
+''' Таблица Мероприятий '''
+class Events(models.Model):
+	title = models.CharField('Название мероприятия', max_length=250)
+	description = models.TextField('Описание мероприятия', db_index=True)
+	date_event = DateField('Дата мероприятия')
+	time_start = TimeField('Начало мероприятия', unique=True)
+	time_end = TimeField('Окончание мероприятия', unique=True)
+	location = models.CharField('Зона проведения', max_length=75)
+	hoster = models.CharField('Участник мероприятия', max_length=75)
+	lector = models.CharField('Ведущий мероприятия', max_length=75)
+	exhibition = models.ForeignKey(Exhibitions, on_delete=models.SET_NULL, related_name='events', null=True, blank=True)
+
+	# Metadata
+	class Meta:
+		ordering = ['title'] # '-' for DESC ordering
+		verbose_name = 'Мероприятие'
+		verbose_name_plural = 'Мероприятия'
 
 	def save(self, *args, **kwargs):
 		if not self.slug:
@@ -210,20 +198,17 @@ class Events(models.Model):
 		pass
 		#return reverse('event-detail', kwargs={'slug': self.slug })
 
+
 class Portfolio(models.Model):
-	owner = models.ForeignKey(Exhibitors, on_delete=models.SET_NULL, related_name='owner')
-	exhibition = models.ForeignKey(Exhibition, on_delete=models.SET_NULL, related_name='exhibition', null=True, blank=True)
+	owner = models.ForeignKey(Exhibitors, on_delete=models.SET_NULL, related_name='exhibitor')
+	exhibition = models.ForeignKey(Exhibitions, on_delete=models.SET_NULL, related_name='exhibition', null=True, blank=True)
 	nomination = models.ManyToManyField(Nominations, on_delete=models.SET_NULL, related_name='nomination')
 	description = models.TextField('Описание работы', db_index=True)
 
-class ExRelationships(models.Model):
-	exhibition = models.ForeignKey(Exhibition, on_delete=models.SET_NULL)
-	nomination = models.ForeignKey(Nominations, on_delete=models.SET_NULL)
-
 
 class Image(models.Model):
-	article = models.ForeignKey(Article, on_delete=models.SET_NULL, related_name='images', null=True, blank=True)
-	title = models.CharField('Название', max_length=150, null=True, blank=True)
+	portfolio = models.ForeignKey(Portfolio, on_delete=models.SET_NULL, related_name='portfolio', null=True, blank=True)
+	title = models.CharField('Заголовок', max_length=150, null=True, blank=True)
 	description = models.CharField('Описание', max_length=255, null=True, blank=True)
 	file = models.ImageField('Фото', upload_to='uploads/', default="/no-image.png", storage=MediaFileStorage(), unique=False)
 	name = models.CharField('Имя файла', max_length=50, null=True)
