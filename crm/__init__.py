94fa3b454477b68022ddeed49c136abbbd3c47ee
Starck43 <saloon.as@gmail.com>
2025-11-29 23:47:31 +0300

    remove sorl import for deployment

--- crm/__init__.py
+++ crm/__init__.py
@@ -1,7 +1,7 @@
-import logging
-from sorl.thumbnail.log import ThumbnailLogHandler
-
-
-handler = ThumbnailLogHandler()
-handler.setLevel(logging.ERROR)
-logging.getLogger('sorl.thumbnail').addHandler(handler)
+# import logging
+# from sorl.thumbnail.log import ThumbnailLogHandler
+#
+#
+# handler = ThumbnailLogHandler()
+# handler.setLevel(logging.ERROR)
+# logging.getLogger('sorl.thumbnail').addHandler(handler)
